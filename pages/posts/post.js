2eddd73d1737387f29b5bb82ebfa7a3c24d765e4
Janine-ZN <Janine_Z@163.com>
2019-08-22 10:02:34 +0800

    列表页带参跳转到详情页

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -3,15 +3,17 @@ var postsData = require('../../data/posts-data.js');
 Page({
   data: {},
   onLoad: function(options) {
+    // this.data.postList = postsData.postList;
     this.setData({
       post_list: postsData.postList
     });
-  }, 
-  onPostTap:function(event){
-
+  },
+  onPostTap: function(event) {
     console.log(event);
+    var postId = event.currentTarget.dataset.postid;
     wx.navigateTo({
-      url: './post-detail/post-detail',
+      // url: './post-detail/post-detail',
+      url: './post-detail/post-detail?id=' + postId,
     });
   }
 })
